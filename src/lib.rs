62edf94603b61a548b77c5ce5f52984b081543c0
Dirreck <mingyang_ge@163.com>
2023-07-26 14:27:28 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -255,6 +255,7 @@ impl Build {
             "armv7-unknown-linux-gnueabihf" => "linux-armv4",
             "armv7-unknown-linux-musleabihf" => "linux-armv4",
             "asmjs-unknown-emscripten" => "gcc",
+            "csky-unknown-linux-gnuabiv2" => "linux-elf",
             "i586-unknown-linux-gnu" => "linux-elf",
             "i586-unknown-linux-musl" => "linux-elf",
             "i686-apple-darwin" => "darwin-i386-cc",
